975a383b144783abf8dcef78036917af9b7b8ffb
yindaheng98 <yindaheng98@gmail.com>
2023-10-22 13:21:27 -0700

    no flip

--- project3_package/lab3-part2-eval.py
+++ project3_package/lab3-part2-eval.py
@@ -197,9 +197,6 @@ def get_instance_sample(data, idx, img=None):
 class PlaneDataset(Dataset):
     def __init__(self, set_name, data_list):
         self.transforms = transforms.Compose([
-            transforms.RandomHorizontalFlip(0.5),
-            transforms.RandomVerticalFlip(0.5),
-            transforms.RandomRotation(45),
             transforms.ToTensor(), # Converting the image to tensor and change the image format (Channels-Last => Channels-First)
             transforms.Normalize((123.675, 116.28, 103.53), (58.395, 57.12, 57.375)),
         ])
@@ -272,8 +269,8 @@ for (img, mask) in tqdm(loader):
     mask = mask.cuda()
     pred = model(img)
     pred = pred[:, 0, :, :].squeeze(1)  # BATCH x 1 x H x W => BATCH x H x W
-    pred = pred > 0.6
-    mask = mask > 0.6
+    pred = pred > 0.5
+    mask = mask > 0.5
     
     intersection = (pred & mask).float().sum((1, 2))  # Will be zero if Truth=0 or Prediction=0
     union = (pred | mask).float().sum((1, 2))         # Will be zzero if both are 0
@@ -304,10 +301,10 @@ with torch.no_grad():
   mask = mask.cuda()
   pred = model(img)
   pred = pred[:, 0, :, :].squeeze(1)  # BATCH x 1 x H x W => BATCH x H x W
-  pred[pred > 0.6] = 255
-  pred[pred <= 0.6] = 0
-  mask[mask > 0.6] = 255
-  mask[mask <= 0.6] = 0
+  pred[pred > 0.5] = 255
+  pred[pred <= 0.5] = 0
+  mask[mask > 0.5] = 255
+  mask[mask <= 0.5] = 0
   mask = mask.cpu().numpy()
   pred = pred.cpu().numpy()
   for k in range(batch_size):
