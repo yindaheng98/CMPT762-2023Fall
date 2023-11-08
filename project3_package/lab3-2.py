c44a8e35af914fa419a3236a8ecf8afe9390a914
Marsh-li <97719633+Marsh-li@users.noreply.github.com>
2023-11-07 22:51:37 -0800

    Get AP value of part1

--- project3_package/lab3-2.py
+++ project3_package/lab3-2.py
@@ -1,4 +1,5 @@
 # import some common libraries
+from typing import Any
 from cv2 import imshow
 from sklearn.metrics import jaccard_score
 from PIL import Image, ImageDraw
@@ -19,10 +20,13 @@ from torch.autograd import Variable
 import torch.nn.functional as F
 import torch.nn as nn
 import torch
+import shutil
 
 # import some common detectron2 utilities
 import detectron2
 from detectron2 import model_zoo
+from detectron2.structures import Boxes
+from detectron2.layers import nms
 from detectron2.config import get_cfg
 from detectron2.structures import BoxMode
 from detectron2.engine import DefaultTrainer
@@ -33,6 +37,7 @@ from detectron2.utils.visualizer import Visualizer
 from detectron2.data import build_detection_test_loader
 from detectron2.data import MetadataCatalog, DatasetCatalog
 from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+from detectron2.utils.visualizer import GenericMask
 setup_logger()
 
 '''
@@ -44,12 +49,28 @@ setup_logger()
 '''
 BASE_DIR = './'
 
-stride = 800
-size = 1500
+COCO_EVALUATOR_OUTPUT = "{}/COCO_output".format(BASE_DIR)
+if os.path.exists(COCO_EVALUATOR_OUTPUT):
+    shutil.rmtree(COCO_EVALUATOR_OUTPUT)
+os.makedirs(COCO_EVALUATOR_OUTPUT, exist_ok=True)
+
+IMAGE_DIR = "{}/images_output".format(BASE_DIR)
+os.makedirs(IMAGE_DIR, exist_ok=True)
+
+data_dirs = '{}/data'.format(BASE_DIR)
+
+cropped_dir = os.path.join(data_dirs, "cropped")
+os.makedirs(cropped_dir, exist_ok=True)
+
+
+stride = 500
+size = 1000
+
 
 def get_image_dimensions(filename):
     with Image.open(filename) as img:
-        return img.size  # returns (width, height)
+        return img.size 
+
 
 def adjust_segmentation(segmentation, i, j):
     adjusted_segmentation = []
@@ -65,56 +86,46 @@ def adjust_segmentation(segmentation, i, j):
 
 def split_image_and_annotations(img, annotations):
     cropped_annotations = []
-    data_dirs = '{}/data'.format(BASE_DIR)
-    cropped_dir = os.path.join(data_dirs, "cropped")
-
     width, height = img.size
+    x, y, w, h = annotations["bbox"]
 
-    index = 1
-    for i in range(0, width, stride):
-        for j in range(0, height, stride):
-            if i + size > width:
-                i = width - size
-            if j + stride > height:
-                j = height - size
+    for i in range(0, width-stride, stride):
+        for j in range(0, height-stride, stride):
+            i_add = size
+            j_add = size
+            if i + i_add > width:
+                i_add = width - i
+            if j + j_add > height:
+                j_add = height - j
             
-            cropped_filename = os.path.join(cropped_dir, "{}_cropped_{}.png".format(annotations["file_name"][:-4], str(index)))
+            cropped_filename = os.path.join(cropped_dir, "{}_{}_{}".format(i,j,annotations["file_name"]))
             if not os.path.exists(cropped_filename):
-                cropped_img = img.crop((i, j, i + size, j + size))
-                cropped_img.save(cropped_filename)
-            index +=1
+                cropped_img = img.crop((i, j, i + i_add, j + j_add))
+                cropped_img.save(cropped_filename,"PNG")
 
-            x, y, w, h = annotations["bbox"]
             if x >= i and (x + w) <= (i + size) and y >= j and (y + h) <= (j + size):
                 cropped_anno = annotations.copy()
                 cropped_anno["bbox"] = [x - i, y - j, w, h]
                 cropped_anno["segmentation"] = adjust_segmentation(annotations["segmentation"],i,j)
-                #cropped_anno["segmentation"] =[[0,0]]
                 cropped_anno["file_name"] = cropped_filename
-                cropped_anno["id"] = annotations["id"]
+                cropped_anno["position"] = [i,j]
+                cropped_anno["width"] = i_add
+                cropped_anno["height"] = j_add
                 cropped_annotations.append(cropped_anno)
-                #print(len(cropped_anno["segmentation"]))
-
     return cropped_annotations
 
+
 def get_detection_data(set_name):
-    data_dirs = '{}/data'.format(BASE_DIR)
-    cropped_dir = os.path.join(data_dirs, "cropped")
-    if not os.path.exists(cropped_dir):
-        os.makedirs(cropped_dir)
-
+    
     dataset = []
-
-    # Load the annotations from the JSON file
     with open(os.path.join(data_dirs, "train.json"), 'r') as f:
       data = json.load(f)
 
-    val_size = int(len(data) * 0.2)
+    val_size = 6274
 
-    train_data = data[:-val_size]
-    val_data = data[-val_size:]
+    train_data = data[:val_size]
+    val_data = data[val_size:]
 
-    # Determine the path to the JSON file based on the set_name
     if set_name == "train":
         annotations = train_data
     elif set_name == "val":
@@ -122,49 +133,19 @@ def get_detection_data(set_name):
     elif set_name == "all":
         annotations = data
     elif set_name == "test":
-        test_dir = os.path.join(data_dirs, "test")
-        for filename in os.listdir(test_dir):
-            if filename.endswith(".jpg") or filename.endswith(".png"):
-                filepath = os.path.join(test_dir, filename)
-                width, height = get_image_dimensions(filepath)
-                record = {
-                    "file_name": filepath,
-                    "image_id": filename,
-                    "height": height,
-                    "width": width,
-                    "annotations": []
-                }
-                dataset.append(record)
-        return dataset
+        annotations = data
     else:
         raise ValueError("Unknown set_name: {}".format(set_name))
 
-
     for anno in annotations:
-        if set_name == "val":
-            filename = os.path.join(data_dirs, "train", anno["file_name"])
-        elif set_name == "all":
-            filename = os.path.join(data_dirs, "train", anno["file_name"])
-        else:
-            filename = os.path.join(data_dirs, set_name, anno["file_name"])
-
-        img = Image.open(filename)
-        cropped_anno = split_image_and_annotations(img, anno)
-        
-        for crop_anno in cropped_anno:
-            #if len(crop_anno["segmentation"]) % 2 != 0:  # 如果segment的长度不是偶数
-                #print(f"Invalid segmentation data found: {segment}")
-
-            filename_crop = crop_anno["file_name"]
-            width, height = get_image_dimensions(filename_crop)
-            
-            # Check if filename already exists in dataset
-            existing_record = next((record for record in dataset if record["file_name"] == filename_crop), None)
-
+        filename = os.path.join(data_dirs, "train", anno["file_name"])
+        if set_name == "test":
+            width, height = get_image_dimensions(filename)
+            existing_record = next((record for record in dataset if record["file_name"] == filename), None)
             if existing_record is None:
                 record = {
-                    "file_name": filename_crop,
-                    "image_id": crop_anno["image_id"],
+                    "file_name": filename,
+                    "image_id": anno["image_id"],
                     "height": height,
                     "width": width,
                     "annotations": []
@@ -173,8 +154,35 @@ def get_detection_data(set_name):
             else:
                 record = existing_record
 
-            # If it's the test set, we don't have annotations
-            if set_name != "test":
+            obj = {
+                "bbox": anno["bbox"],
+                "bbox_mode": BoxMode.XYWH_ABS,
+                "segmentation": anno["segmentation"],
+                "category_id": anno["category_id"],
+                "iscrowd": anno["iscrowd"],
+                "area": anno["area"]
+            }
+            record["annotations"].append(obj)
+        else: 
+            img = Image.open(filename)
+            cropped_anno = split_image_and_annotations(img, anno)
+            
+            for crop_anno in cropped_anno:
+                filename_crop = crop_anno["file_name"]
+                existing_record = next((record for record in dataset if record["file_name"] == filename_crop), None)
+                if existing_record is None:
+                    record = {
+                        "file_name": filename_crop,
+                        "image_id": crop_anno["image_id"],
+                        "height": crop_anno["height"],
+                        "width": crop_anno["width"],
+                        "position": crop_anno["position"],
+                        "annotations": []
+                    }
+                    dataset.append(record)
+                else:
+                    record = existing_record
+
                 obj = {
                     "bbox": crop_anno["bbox"],
                     "bbox_mode": BoxMode.XYWH_ABS,
@@ -185,7 +193,6 @@ def get_detection_data(set_name):
                 }
 
                 record["annotations"].append(obj)
-
     return dataset
 
 
@@ -199,21 +206,22 @@ for d in ["train", "test", "val", "all"]:
     DatasetCatalog.register("plane_" + d, lambda d=d: get_detection_data(d))
     MetadataCatalog.get("plane_" + d).set(thing_classes=["class1","class2","class3","class4","plane"])
 
-plane_metadata = MetadataCatalog.get("plane_train")
-
-# Load some samples from the training dataset
-samples = get_detection_data("train")
+plane_metadata = MetadataCatalog.get("plane_all")
 
 
 '''
 # Visualize some samples using Visualizer to make sure that the function works correctly
 # TODO: approx 5 lines
 '''
-for idx, d in enumerate(random.sample(samples, 5)):
+# Load some samples from the training dataset
+samples = get_detection_data("all")
+
+for idx, d in enumerate(random.sample(samples, 1)):
     img = cv2.imread(d["file_name"])
+    # print(d["file_name"])
     visualizer = Visualizer(img[:, :, ::-1], metadata=plane_metadata, scale=0.5)
     out = visualizer.draw_dataset_dict(d)
-    save_path = f"output/Q1_GT_{idx}.jpg"
+    save_path = IMAGE_DIR+f"/Q1_GT_{idx}.jpg"
     cv2.imwrite(save_path, out.get_image()[:, :, ::-1])
     print(f"Image saved to {save_path}")
 
@@ -227,26 +235,25 @@ cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_101_
 
 cfg.OUTPUT_DIR = "{}/output/".format(BASE_DIR)
 
-cfg.DATASETS.TRAIN = ("plane_train",)
+cfg.DATASETS.TRAIN = ("plane_all",)
 cfg.DATASETS.TEST = ()
 
-cfg.SOLVER.MAX_ITER = 500             # Number of training iterations
+cfg.SOLVER.MAX_ITER = 1000             # Number of training iterations
 cfg.SOLVER.BASE_LR = 0.00025          # Learning rate
 cfg.SOLVER.IMS_PER_BATCH = 2          # Number of images per batch
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512  # Number of regions per image for training
-
-# 4. Pretrain Model (Improve from 25% to 47%)
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml")
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+# 4. Pretrain Model
+cfg.MODEL.WEIGHTS = "{}/500_detection_model.pth".format(cfg.OUTPUT_DIR)
 
 
 '''
 # Create a DefaultTrainer using the above config and train the model
 # TODO: approx 5 lines
 '''
-os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
-trainer = DefaultTrainer(cfg)
-trainer.resume_or_load(resume=False)
-trainer.train()
+#trainer = DefaultTrainer(cfg)
+#trainer.resume_or_load(resume=False)
+#trainer.train()
 
 
 '''
@@ -262,33 +269,131 @@ predictor = DefaultPredictor(cfg)
 # Visualize the output for 3 random test samples
 # TODO: approx 10 lines
 '''
-dataset_dicts = get_detection_data("test")
-for idx, d in enumerate(random.sample(dataset_dicts, 3)):    
-    im = cv2.imread(d["file_name"])
-    outputs = predictor(im)  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
-    v = Visualizer(im[:, :, ::-1],
-                   metadata=plane_metadata, 
-                   scale=0.5, 
-                   instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels. This option is only available for segmentation models
-    )
-    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-    save_path = f"output/Q1_PD_{idx}.jpg"
-    cv2.imwrite(save_path, out.get_image()[:, :, ::-1])
-    print(f"Image saved to {save_path}")
+def calculate_iou(box1, box2):
+    # 计算两个框的交集坐标
+    x1 = max(box1[0], box2[0])
+    y1 = max(box1[1], box2[1])
+    x2 = min(box1[2], box2[2])
+    y2 = min(box1[3], box2[3])
 
+    # 计算交集的面积
+    intersection_area = max(0, x2 - x1) * max(0, y2 - y1)
+
+    # 计算两个框各自的面积
+    box1_area = (box1[2] - box1[0]) * (box1[3] - box1[1])
+    box2_area = (box2[2] - box2[0]) * (box2[3] - box2[1])
+
+    # 计算重叠的面积占各自框面积的比例
+    iou_box1 = intersection_area / box1_area
+    iou_box2 = intersection_area / box2_area
+
+    return iou_box1, iou_box2
+
+
+def filter_overlap_bbox(dataset_pred, iou_threshold=0.6, overlap_ratio=0.7, size_ratio=0.01):
+    dataset_pred = dataset_pred
+    for idx, d in enumerate(dataset_pred): 
+        boxes = d["instances"][0]["instances"].pred_boxes.tensor
+        scores = d["instances"][0]["instances"].scores
+
+
+        areas = (boxes[:, 2] - boxes[:, 0]) * (boxes[:, 3] - boxes[:, 1])
+        average_area = areas.mean()
+
+        keep_size = areas >= (average_area * size_ratio)
+        boxes = boxes[keep_size]
+        scores = scores[keep_size]
+
+        keep = nms(boxes, scores, iou_threshold=iou_threshold)
+        boxes = boxes[keep]
+        scores = scores[keep]
+
+        keep_mask = torch.ones(boxes.size(0), dtype=torch.bool)
+
+        for i in range(boxes.size(0)):
+            if not keep_mask[i]:
+                continue
+            for j in range(boxes.size(0)):
+                if i != j and keep_mask[j]:
+                    iou_box1, iou_box2 = calculate_iou(boxes[i], boxes[j])
+
+                    if iou_box1 > overlap_ratio or iou_box2 > overlap_ratio:
+                        if scores[i] < scores[j]:
+                            keep_mask[i] = False
+                        else:
+                            keep_mask[j] = False
+        keep = keep[keep_mask]
+        d["instances"][0]["instances"] = d["instances"][0]["instances"][keep]
+
+    return dataset_pred
+
+
+def predict_and_adjust_bbox(dataset_dicts):
+    all_predictions = []
+    for idx, d in enumerate(dataset_dicts):
+        im = cv2.imread(d["file_name"])
+        outputs = predictor(im)
+        file_number = d["file_name"][-9:-4]
+        scores = outputs["instances"].scores
+        keep = scores>0.6
+        outputs["instances"] = outputs["instances"][keep]
+
+        dx, dy = d["position"]
+        boxes = outputs["instances"].pred_boxes.tensor
+        boxes[:, 0::2] += dx
+        boxes[:, 1::2] += dy
+        outputs["instances"].pred_boxes = Boxes(boxes)
+
+
+        all_predictions.append({
+            "predictions": outputs,
+            "file_number": file_number,
+        })
+    return all_predictions
+
+
+def create_predict_result(dataset_dicts):
+    dataset_pred = []
+    all_predictions = predict_and_adjust_bbox(dataset_dicts)
+    for idx in range(len(all_predictions)):
+        file_name = os.path.join(data_dirs, "train", "{}.png".format(all_predictions[idx]["file_number"]))
+
+        existing_record = next((record for record in dataset_pred if record["file_name"] == file_name), None)
+
+        if existing_record is None:
+            record = {
+                "file_name": file_name,
+                "instances": [all_predictions[idx]["predictions"]]
+            }
+            dataset_pred.append(record)
+        else:
+            record = existing_record
+            width, height = get_image_dimensions(file_name)
+            data_record = record["instances"][0]["instances"]
+            data_new = all_predictions[idx]["predictions"]["instances"]
+
+            boxes = torch.cat((data_record.pred_boxes.tensor, data_new.pred_boxes.tensor), dim=0)
+            boxes = Boxes(boxes)
+            scores = torch.cat((data_record.scores, data_new.scores), dim=0)
+            pred_classes = torch.cat((data_record.pred_classes, data_new.pred_classes), dim=0)
+            
+            new_instance = detectron2.structures.Instances(image_size=(height, width))
+            new_instance.set('pred_boxes', boxes)
+            new_instance.set('scores', scores)
+            new_instance.set('pred_classes', pred_classes)
+            record["instances"][0]["instances"] = new_instance
+
+    return filter_overlap_bbox(dataset_pred)
+
+dataset_dicts = get_detection_data("all")
+dataset_pred = create_predict_result(dataset_dicts)
 
 '''
 # Use COCOEvaluator and build_detection_train_loader
 # You can save the output predictions using inference_on_dataset
 # TODO: approx 5 lines
 '''
-evaluator = COCOEvaluator("plane_val", output_dir="./output")
-val_loader = build_detection_test_loader(cfg, "plane_val")
-print(inference_on_dataset(predictor.model, val_loader, evaluator))
-# another equivalent way to evaluate the model is to use `trainer.test`
+evaluator = COCOEvaluator("plane_test", output_dir="./COCO_output")
+val_loader = build_detection_test_loader(cfg, "plane_test")
+print(inference_on_dataset(dataset_pred, predictor.model, val_loader, evaluator))
 
-
-
-'''
-# Bring any changes and updates regarding the improvement in here
-'''
