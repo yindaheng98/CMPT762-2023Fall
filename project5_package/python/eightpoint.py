284097a69379a4ffbbc67d5c864850bcb382c81f
yindaheng98 <yindaheng98@gmail.com>
2023-11-27 16:42:07 -0800

    3.1.1 over

--- project5_package/python/eightpoint.py
+++ project5_package/python/eightpoint.py
@@ -13,10 +13,13 @@ def eightpoint(pts1, pts2, M):
     
     # Implement the eightpoint algorithm
     # Generate a matrix F from correspondence '../data/some_corresp.npy'
-    # pts1_xyz = np.concatenate([pts1, np.zeros((pts1.shape[0], 1)) + M], axis=1) # same as divide by M
-    # pts2_xyz = np.concatenate([pts2, np.zeros((pts2.shape[0], 1)) + M], axis=1) # same as divide by M
+
+    # pts1_xyz = np.concatenate([pts1, np.ones((pts1.shape[0], 1))], axis=1) # what we really should do, but unstable?
+    # pts2_xyz = np.concatenate([pts2, np.ones((pts2.shape[0], 1))], axis=1) # what we really should do, but unstable?
     pts1_xyz = np.concatenate([pts1 / M, np.ones((pts1.shape[0], 1))], axis=1)
     pts2_xyz = np.concatenate([pts2 / M, np.ones((pts2.shape[0], 1))], axis=1)
+    # pts1_xyz = np.concatenate([pts1, np.zeros((pts1.shape[0], 1)) + M], axis=1) # same as divide by M
+    # pts2_xyz = np.concatenate([pts2, np.zeros((pts2.shape[0], 1)) + M], axis=1) # same as divide by M
     A = np.reshape(np.stack([pts1_xyz] * 3, axis=2) * np.stack([pts2_xyz] * 3, axis=1), (-1, 9))
     SVDResults = svd(A)
     U, S, V = SVDResults.U, SVDResults.S, SVDResults.Vh
@@ -26,4 +29,5 @@ def eightpoint(pts1, pts2, M):
     U, S, V = SVDResults.U, SVDResults.S, SVDResults.Vh
     S[-1] = 0
     F = np.dot(U * S.reshape((1, -1)), V)
-    return F
+    T = np.array([1/M, 1/M, 1])
+    return T.reshape((-1, 1)) * F * T.reshape((1, -1))
