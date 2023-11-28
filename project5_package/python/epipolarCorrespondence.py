6edac3532a823960d577a5f677d83e1221d90e3f
yindaheng98 <yindaheng98@gmail.com>
2023-11-28 15:37:57 -0800

    bug fixed

--- project5_package/python/epipolarCorrespondence.py
+++ project5_package/python/epipolarCorrespondence.py
@@ -1,14 +1,23 @@
 import numpy as np
 import cv2
 
-patch_size = 5
+patch_size = 3
+
 
 def get_patch(im, x, y, patch_size):
     x, y = int(np.floor(x)), int(np.floor(y))
     y_max, x_max = im.shape[:2]
     xd, xt = max(0, x-patch_size), min(x+patch_size, x_max)
     yd, yt = max(0, y-patch_size), min(y+patch_size, y_max)
-    return im[yd:yt, xd:xt]
+    patch = np.zeros((patch_size * 2 + 1, patch_size * 2 + 1, 3))
+    xd_, xt_ = max(patch_size-x, 0), patch_size + min(x_max-x, patch_size)
+    yd_, yt_ = max(patch_size-y, 0), patch_size + min(y_max-y, patch_size)
+    patch[yd_:yt_, xd_:xt_, :] = im[yd:yt, xd:xt, :]
+    return patch
+
+
+def diff_patch(a, b):
+    return np.mean((a - a.mean()) * (b - b.mean())) / (a.std() * b.std())
 
 
 def epipolarCorrespondence(im1, im2, F, pts1):
@@ -25,18 +34,18 @@ def epipolarCorrespondence(im1, im2, F, pts1):
     l = np.dot(np.array([xl, yl, 1]), F.T)
 
     yr_max, xr_max = im2.shape[:2]
-    if l[0] < l[1]:
+    if l[0] > l[1]:
         yr = np.array(list(range(yr_max)))
         xr = -(l[1] * yr + l[2]) / l[0]
     else:
         xr = np.array(list(range(xr_max)))
         yr = -(l[0] * xr + l[2]) / l[1]
-    xr, yr = xr[xr>=patch_size], yr[xr>=patch_size]
-    xr, yr = xr[yr>=patch_size], yr[yr>=patch_size]
-    xr, yr = xr[xr<xr_max-patch_size], yr[xr<xr_max-patch_size]
-    xr, yr = xr[yr<yr_max-patch_size], yr[yr<yr_max-patch_size]
-    patch_l = get_patch(im1, xl, yl, patch_size)
-    diff = np.array([np.sum(np.abs(patch_l-get_patch(im2, x, y, patch_size))) for x, y in zip(xr, yr)])
-    # distance = np.abs(xr-xl) ** 2 + np.abs(yr-yl) ** 2
-    # diff[distance>2048] = np.max(diff)
-    return np.array([[xr[np.argmin(diff)], yr[np.argmin(diff)]]])
+    xr, yr = xr[xr >= patch_size], yr[xr >= patch_size]
+    xr, yr = xr[yr >= patch_size], yr[yr >= patch_size]
+    xr, yr = xr[xr < xr_max-patch_size], yr[xr < xr_max-patch_size]
+    xr, yr = xr[yr < yr_max-patch_size], yr[yr < yr_max-patch_size]
+
+    im1_YCrCb, im2_YCrCb = cv2.cvtColor(im1, cv2.COLOR_BGR2YCrCb), cv2.cvtColor(im2, cv2.COLOR_BGR2YCrCb)
+    patch_l = get_patch(im1_YCrCb, xl, yl, patch_size)
+    diff = np.array([diff_patch(patch_l, get_patch(im2_YCrCb, x, y, patch_size)) for x, y in zip(xr, yr)])
+    return np.array([[xr[np.argmax(diff)], yr[np.argmax(diff)]]])
