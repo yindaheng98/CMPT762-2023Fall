c089c9c26c71d3a1527238ec856ba2b0d957c8e1
yindaheng98 <yindaheng98@gmail.com>
2023-11-27 18:38:55 -0800

    basic search

--- project5_package/python/epipolarCorrespondence.py
+++ project5_package/python/epipolarCorrespondence.py
@@ -1,6 +1,16 @@
 import numpy as np
 import cv2
 
+patch_size = 5
+
+def get_patch(im, x, y, patch_size):
+    x, y = int(np.floor(x)), int(np.floor(y))
+    y_max, x_max = im.shape[:2]
+    xd, xt = max(0, x-patch_size), min(x+patch_size, x_max)
+    yd, yt = max(0, y-patch_size), min(y+patch_size, y_max)
+    return im[yd:yt, xd:xt]
+
+
 def epipolarCorrespondence(im1, im2, F, pts1):
     """
     Args:
@@ -21,10 +31,16 @@ def epipolarCorrespondence(im1, im2, F, pts1):
     else:
         xr = np.array(list(range(xr_max)))
         yr = -(l[0] * xr + l[2]) / l[1]
-    xr_, yr_ = xr, yr
-    xr, yr = xr[xr>=0], yr[xr>=0]
-    xr, yr = xr[yr>=0], yr[yr>=0]
-    xr, yr = xr[xr<xr_max], yr[xr<xr_max]
-    xr, yr = xr[yr<yr_max], yr[yr<yr_max]
-    pts2 = np.array([[xr[0], yr[0]]])
-    return pts2
+    xr, yr = xr[xr>=patch_size], yr[xr>=patch_size]
+    xr, yr = xr[yr>=patch_size], yr[yr>=patch_size]
+    xr, yr = xr[xr<xr_max-patch_size], yr[xr<xr_max-patch_size]
+    xr, yr = xr[yr<yr_max-patch_size], yr[yr<yr_max-patch_size]
+    patch_l = get_patch(im1, xl, yl, patch_size)
+    min_diff, x_best, y_best = np.sum(np.abs(patch_l-get_patch(im2, xr[0], yr[0], patch_size))), xr[0], yr[0]
+    for x, y in zip(xr[1:], yr[1:]):
+        diff = np.sum(np.abs(patch_l-get_patch(im2, x, y, patch_size)))
+        if diff < min_diff:
+            min_diff = diff
+            x_best = x
+            y_best = y
+    return np.array([[x_best, y_best]])
