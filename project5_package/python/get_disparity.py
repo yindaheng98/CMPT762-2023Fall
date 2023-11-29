9bf3b531555e22f87bf841d937ed8d99536f9ac5
yindaheng98 <yindaheng98@gmail.com>
2023-11-29 00:27:31 -0800

    much faster

--- project5_package/python/get_disparity.py
+++ project5_package/python/get_disparity.py
@@ -1,6 +1,30 @@
 import numpy as np
-from itertools import product
-from epipolarCorrespondence import get_patch, diff_patches
+from epipolarCorrespondence import get_patch
+import tqdm
+
+
+def diff_patches(patch, patches, maxDisp):
+    patch_ = patch.reshape((patch.shape[0], -1))
+    patch_mean = patch_.mean(axis=1)
+    patch_std = patch_.std(axis=1)
+    patch_std[patch_std < 1e-6] = 1e-6
+
+    patches_ = patches.reshape((patches.shape[0], -1))
+    patches_mean = patches_.mean(axis=1)
+    patches_std = patches_.std(axis=1)
+    patches_std[patches_std < 1e-6] = 1e-6
+
+    mean_ = (patch_.T - patch_mean).T
+    means_ = (patches_.T - patches_mean).T
+    w = maxDisp//2
+    x_max = min(mean_.shape[0], means_.shape[0])-w
+    means_split = np.array([means_[x-w:x+w, :] for x in range(w, x_max)])
+    stds_split = np.array([patches_std[x-w:x+w] for x in range(w, x_max)])
+    mean = np.mean(mean_[w:x_max, :] * means_split.transpose((1, 0, 2)), axis=2)
+    std = (patch_std[w:x_max] * stds_split.T)
+    std[std < 1e-6] = 1e-6
+
+    return (mean / std).T
 
 
 def get_disparity(im1, im2, maxDisp, windowSize):
@@ -12,9 +36,12 @@ def get_disparity(im1, im2, maxDisp, windowSize):
     w = (windowSize - 1) // 2
     y1_max, x1_max = im1.shape[:2]
     y2_max, x2_max = im2.shape[:2]
-    for (x1, y) in product(range(x1_max), range(min(y1_max, y2_max))):
-        patch_l = get_patch(im1, x1, y, w)
-        patch_r = np.array([get_patch(im1, x2, y, w) for x2 in range(max(0, x1 - maxDisp), min(x1 + maxDisp, x2_max))])
-        diff = diff_patches(patch_l, patch_r)
-        dispM[y, x1] = x1 - np.argmin(diff)
+    for y in range(min(y1_max, y2_max)):
+        x1s = np.array(list(range(x1_max)))
+        patch_l = np.array([get_patch(im1, x1, y, w) for x1 in x1s])
+        patch_r = np.array([get_patch(im2, x2, y, w) for x2 in range(0, x2_max)])
+        diff = diff_patches(patch_l, patch_r, maxDisp)
+        x_min, x_max = maxDisp//2, min(x1_max, x2_max)-maxDisp//2
+        dispM[y, x_min:x_max] = x1s[x_min:x_max] - np.argmax(diff, axis=1)
+        dispM[np.sum(diff) == 0, :] = 0
     return dispM
