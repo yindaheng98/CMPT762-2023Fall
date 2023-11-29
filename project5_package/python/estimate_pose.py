dd85e0f2219d07b7a0aebc00cc6f6555c9e09c5a
yindaheng98 <yindaheng98@gmail.com>
2023-11-29 15:44:29 -0800

    estimate_pose

--- project5_package/python/estimate_pose.py
+++ project5_package/python/estimate_pose.py
@@ -10,6 +10,11 @@ def estimate_pose(x, X):
         x: 2D points with shape [2, N]
         X: 3D points with shape [3, N]
     """
-    P = None
-    
+    X = np.concatenate([X, np.ones((1, *X.shape[1:]))])
+    Al = np.stack([np.concatenate([X, np.zeros_like(X)]), np.concatenate([np.zeros_like(X), X])], axis=1)
+    Ar = -x * np.stack([X] * x.shape[0], axis=1)
+    A = np.concatenate([Al, Ar]).reshape(((X.shape[0] + x.shape[0]) * 2, x.shape[0] * X.shape[1], *X.shape[2:]), order='F').T
+    U, s, Vh = svd(A)
+    v = Vh[np.argmin(s)]
+    P = v.reshape((3, 4))
     return P
