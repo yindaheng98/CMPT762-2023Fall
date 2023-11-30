34aec08d38a85f6eda45eeaecf1689262f81ddf4
yindaheng98 <yindaheng98@gmail.com>
2023-11-29 20:13:42 -0800

    estimate_params

--- project5_package/python/estimate_params.py
+++ project5_package/python/estimate_params.py
@@ -9,7 +9,14 @@ def estimate_params(P):
     Args:
         P: Camera matrix
     """
-    K, R, t = None, None, None
-    
+    H, h = P[:, 0:3], P[:, 3]
+    q, r = qr(np.linalg.inv(H))
+    R, K = q.T, np.linalg.inv(r)
+    K = K/K[2, 2]
+    R_pi = np.diag([K[i, i]/K[i, i] for i in range(K.shape[0])])
+    K = np.dot(K, R_pi)
+    R = np.dot(R_pi, R)
+    K[0, 1] = 0
+    t = np.dot(h, np.linalg.inv(K).T)
     return K, R, t
 
