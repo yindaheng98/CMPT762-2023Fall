5e953ac0affc8a46f14645fc15a36b23f05c0327
yindaheng98 <yindaheng98@gmail.com>
2023-11-30 21:15:41 -0800

    clear

--- project5_package/python/get_depth2.py
+++ project5_package/python/get_depth2.py
@@ -7,10 +7,11 @@ def Get3dCoord(pts2d, extrinsic, depths):
     n_pts, n_depths = pts2d.shape[0], depths.shape[0]
     K, R, t = extrinsic
     P = np.dot(K, np.concatenate((R.T, [t])).T)
-    dpts2d = np.stack([pts2d] * depths.shape[0], axis=1) * np.stack([depths] * pts2d.shape[1], axis=1)
-    dpts2d3 = np.concatenate([dpts2d, np.expand_dims(np.zeros(dpts2d.shape[0:2]) + depths, axis=2)], axis=2)
+    pts2d_depths = np.stack([pts2d] * depths.shape[0], axis=1) * np.stack([depths] * pts2d.shape[1], axis=1)
+    pts2dz = np.expand_dims(np.zeros(pts2d_depths.shape[0:2]) + depths, axis=2)
+    pts2dxyz = np.concatenate([pts2d_depths, pts2dz], axis=2)
     pts3d = np.dot(
-        dpts2d3.reshape((n_pts * n_depths, 3)) - P[:, 3],
+        pts2dxyz.reshape((n_pts * n_depths, 3)) - P[:, 3],
         np.linalg.inv(P[:, 0:3]).T
     ).reshape((n_pts, n_depths, 3))
     return pts3d
