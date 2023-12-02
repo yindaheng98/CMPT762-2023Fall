0366b4d1339ac1dcb4b0b104d75ac4cfb78ea28d
yindaheng98 <yindaheng98@gmail.com>
2023-12-02 14:30:33 -0800

    final

--- project5_package/python/showPoints.py
+++ project5_package/python/showPoints.py
@@ -3,5 +3,5 @@ import open3d as o3d
 pcd = o3d.io.read_point_cloud("../results/temple.pcd")
 mat = o3d.visualization.rendering.MaterialRecord()
 mat.shader = 'defaultUnlit'
-mat.point_size = 1.5
+mat.point_size = 2.0
 o3d.visualization.draw([{'name': 'pcd', 'geometry': pcd, 'material': mat}], bg_color=(0, 0, 0, 0))
